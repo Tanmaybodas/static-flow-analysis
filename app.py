3ba2dfe5f1d2236bb8abe1044291d5d8f46f449a
Tanmay Mandar Bodas <tanmaybodas19@gmail.com>
2026-01-15 22:25:46 +0530

    new file

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 def hello():
     print("Hello World")
+
+def execute(query):
+    print(query)
+
+name = input("Enter name: ")
+query = "SELECT * FROM users WHERE name = '" + name + "'"
+execute(query)
 hello()
+
