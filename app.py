f03324cafb927410c164616365f5ebea0ba51380
Tanmay Mandar Bodas <tanmaybodas19@gmail.com>
2026-01-15 23:03:56 +0530

    new

--- app.py
+++ app.py
@@ -3,16 +3,3 @@ def hello():
     print("Hello World")
 
 
-def get_user(user_id):
-    conn = sqlite3.connect("users.db")
-    cursor = conn.cursor()
-
-    # ❌ SQL Injection vulnerability
-    query = "SELECT * FROM users WHERE id = " + user_id
-    cursor.execute(query)
-
-get_user("1 OR 1=1")
-
-hello()
-eval("2 + 2")
-
