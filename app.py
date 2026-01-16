db6bc9444ea54b61486c1af78e151afef59cb06d
Tanmay Mandar Bodas <tanmaybodas19@gmail.com>
2026-01-16 10:02:05 +0530

    new

--- app.py
+++ app.py
@@ -9,3 +9,17 @@ query = "SELECT * FROM users WHERE name = '" + name + "'"
 execute(query)
 hello()
 eval("2 + 2")
+
+#
+#
+# def add():
+#     return eval("10 + 20")
+# add()
+#
+#
+# def execute(query):
+#     print(query)
+#
+# name = "admin"
+# query = "SELECT * FROM users WHERE name = '" + name + "'"
+# execute(query)
